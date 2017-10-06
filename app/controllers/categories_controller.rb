948b70aa13478113da64454c0e7963fe953bb10a
Jonathan Weyermann <jonathan.weyermann@ama.ab.ca>
2017-10-06 16:07:41 -0600

    preview the page from admin and hide it from front categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
   def show
   	@category = Category.find(params[:id])
     @categories = Category.all
-    @posts = @category.posts
+    @posts = @category.posts.production
     @users = User.all
   end
 end
