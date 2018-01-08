e1c681182d48ae23bdf1ed25e30dd4b3b068a80d
jonathan <weyermannx@gmail.com>
2018-01-07 22:46:18 -0700

    more intelligent coloring

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -5,11 +5,20 @@ class PostDecorator < Draper::Decorator
     object.visible? ? "green-background" : "red-background"
   end
 
+  def image_present_color
+    object.image.present? ? "green-background" : "red-background"
+  end
+
   def publish_button
     object.production? ? "green-background" : "red-background"
   end
 
   def on_or_off
     object.production? ? "pubed" : "unpubed"
+  end
+
+  def enough_chars
+    return "green-background" if object.body.length > 4000
+    object.body.length < 2500 ? "red-background" : "orange-background"
   end
 end
