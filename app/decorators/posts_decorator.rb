e6a4a0b1671449496c4c0b22fbad207c0ffe4704
jonathan <weyermannx@gmail.com>
2019-12-08 23:04:52 -0700

    replace image zoom with slight post zoom, remove from latest posts if it's the current one

--- app/decorators/posts_decorator.rb
+++ app/decorators/posts_decorator.rb
@@ -7,6 +7,14 @@ class PostsDecorator < Draper::CollectionDecorator
     end
   end
 
+  def visible_except_current(current_id)
+    if any_posts?
+      h.render partial: 'posts/post_slice', collection: exception_slice(current_id), as: :slice
+    else
+      h.raw "<p> No Posts yet </p>"
+    end
+  end
+
   def pagination_bar
     h.render partial: 'partials/pagination_bar', locals: { paginatable: object } if any_posts?
   end
@@ -19,5 +27,9 @@ class PostsDecorator < Draper::CollectionDecorator
 
   def slices
     self.each_slice(2).map {|x| x}
+  end
+
+  def exception_slice(id)
+    object.where.not(id: id).each_slice(2).map {|x| x}
   end
 end
