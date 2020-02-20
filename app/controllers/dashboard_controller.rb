6c7beadb6425f220e2d426fd8c2d11beba9a66fe
jonathan <weyermannx@gmail.com>
2020-02-20 00:41:45 -0700

    move up to 9

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   caches_page :index
   expose(:portfolios) { Portfolio.all }
-  expose(:portfolios_presenter) { PortfoliosDecorator.decorate(portfolios.sorted_public.first(6)) }
+  expose(:portfolios_presenter) { PortfoliosDecorator.decorate(portfolios.sorted_public.first(9)) }
   expose(:posts) { Post.public(params[:page]).limit(4) }
   expose(:posts_presenter) { PostsDecorator.decorate(posts) }
   expose :portfolio
